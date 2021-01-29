676313cb7537bf9d96254bfaf429315f19b44efd
xujie <xujie@langcoo.com>
2021-01-29 14:08:36 +0800

    first commit

---
+++ app/src/main/java/com/xuj/MainActivity.java
@@ -0,0 +1,17 @@
+package com.xuj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xuj.lib.ToastUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ToastUtil.showShort(this,"");
+    }
+}
